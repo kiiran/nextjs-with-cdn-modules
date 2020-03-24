255b6479b920bcaed423ac9f5eea1ce56f8b3bbb
Ciaran Edwards <hello@ciaran.codes>
2020-03-24 09:34:00 +0000

    Clean version number before checking it

--- next.config.js
+++ next.config.js
@@ -2,20 +2,23 @@ const DynamicCdnWebpackPlugin = require('dynamic-cdn-webpack-plugin')
 const moduleToCDN = require('module-to-cdn')
 const packageJson = require('./package.json')
 
+const env = process.env.NODE_ENV
+
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     // Important: return the modified config
 
-    config.plugins.push(new DynamicCdnWebpackPlugin())
+    config.plugins.push(new DynamicCdnWebpackPlugin({ env }))
     
     return config
   },
   serverRuntimeConfig: {
     cdnDependencies: Object.entries(packageJson.dependencies)
-      .map(([package, version]) => {
-        return moduleToCDN(package, version, { env: process.env.NODE_ENV })
+      .map(([dep, ver]) => {
+        const version = ver.replace(/^(\^|@|~)/, '')
+        return moduleToCDN(dep, version, { env })
       })
       .filter(Boolean),
   },
